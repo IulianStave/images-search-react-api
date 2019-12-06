2ff0e5f71ecc6353e86e5e905260076d9d2a5884
iulianstave <iulian.staverescu@gmail.com>
2019-12-06 13:51:41 +0200

    Start a clean React App

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// Pics app - User input via React app
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+
+  render () {
+    return <div><h1>React App Fresh start</h1></div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
