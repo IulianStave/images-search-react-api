2f73b3147cef6955d9755d089d73f8fd18074a45
iulianstave <iulian.staverescu@gmail.com>
2019-12-13 00:50:24 +0200

    List images using map in ListImage comp

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ class App extends React.Component {
   state = {images: [] };
   //define the arrow function async
   onSearchSubmit = async (term) => {
-    // npm install -- save axios
+    // npm install --save axios
     // API endpoint GET /search/photos
     // make a request over the unsplash API using 
     // third party library axios
@@ -28,7 +28,7 @@ class App extends React.Component {
       {/* the prop name 'onSubmit' could be whatever we choose  */}
       <SearchBar onSubmit={this.onSearchSubmit}/>
       {/* Found: {this.state.images.length} images */}
-      <ImageList />
+      <ImageList images = {this.state.images} />
     </div>
     );
   }
--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const ImageList = () => {
-  return <div> ImageList</div>;
+const ImageList = props => {
+  const images = props.images.map((image) => {
+    return <img src={image.urls.regular} />
+  });
+  return <div> {images} </div>;
 };
 
 export default ImageList;
