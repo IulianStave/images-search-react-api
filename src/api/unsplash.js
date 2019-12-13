70b7396533bd86f8ea7910d272d29ad5f11ee058
iulianstave <iulian.staverescu@gmail.com>
2019-12-13 08:56:02 +0200

    Clean unsplash code

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -1,19 +1,11 @@
 // code related to configuration of axios,
-// to unsplash to accept our request
+// Creates an axios object which send a request to unsplash
 import axios from 'axios';
-
-/*
-//an alternative
-const searchImages = (term) => {
-
-}
-
-*/
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: 
+    Authorization:
       'Client-ID b77df092bedc33ccf66e908bac6f01ac2aa906c7a73c0acb4f46b1fc7aab6114'
   }
 });
