8ae6478fe119900602b450b1b830f2d1860267d6
iulianstave <iulian.staverescu@gmail.com>
2019-12-15 19:50:30 +0200

    Arrange images with spans state

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -5,6 +5,7 @@ class ImageCard extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = { spans: 0 };
     this.imageRef = React.createRef();
   }
 
@@ -16,14 +17,16 @@ class ImageCard extends React.Component {
 
   setSpans = () => {
     // when we get that height we use it to determine the img size 
-    console.log(this.imageRef.current.clientHeight);
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+    this.setState({ spans })
   }
 
   render() {
     const { description, urls } = this.props.image;
 
     return (
-      <div>
+      <div style ={{ gridRowEnd: `span ${this.state.spans}`}}>
         <img
           ref={this.imageRef}
           alt={description}
