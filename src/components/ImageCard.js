ac465cf88c1c8a3b1cbbc3e673e17a659483ecf8
iulianstave <iulian.staverescu@gmail.com>
2019-12-15 19:24:07 +0200

    Add Ref to image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,19 +1,26 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  // use Ref system to handle img
+  constructor(props) {
+    super(props);
 
-    render() {
-      const {description, urls} = this.props.image;
+    this.imageRef = React.createRef();
+  }
 
-      return (
-          <div>
-              <img
-                  alt={description}
-                  src={urls.regular}
-              />
-          </div>
-      );
-    }
+  render() {
+    const { description, urls } = this.props.image;
+
+    return (
+      <div>
+        <img
+          ref={this.imageRef}
+          alt={description}
+          src={urls.regular}
+        />
+      </div>
+    );
+  }
 }
 
 export default ImageCard;
