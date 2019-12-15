056bb6c742bf346272589fb51d83e0bd99c202dc
iulianstave <iulian.staverescu@gmail.com>
2019-12-15 19:08:17 +0200

    Deconstructure out this.props.image

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,14 +3,16 @@ import React from 'react';
 class ImageCard extends React.Component {
 
     render() {
-        return (
-            <div>
-                <img
-                    alt={this.props.image.description}
-                    src={this.props.image.urls.regular}
-                />
-            </div>
-        );
+      const {description, urls} = this.props.image;
+
+      return (
+          <div>
+              <img
+                  alt={description}
+                  src={urls.regular}
+              />
+          </div>
+      );
     }
 }
 
